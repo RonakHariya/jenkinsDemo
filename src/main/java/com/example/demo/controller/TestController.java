dd6ccf645e666904caa57aa01331f09050caf7fd
RonakHariya <ronak.hariya@avisys.in>
2024-02-27 12:00:49 +0530

    Test Build

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,45 +1,43 @@
-package com.jenkins.controller;
+package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("jenkins")
 public class TestController {
 
-    // Get Test
-    @GetMapping("/test")
-    public String getTest() {
-        return "GET request successful";
+    @GetMapping("test")
+    public ResponseEntity<String> getTest() {
+        return new ResponseEntity<>("GET request successful",HttpStatus.OK);
     }
 
-    // To Create
-    @PostMapping("/test")
+    @PostMapping("test")
     public String createTest() {
         return "POST request successful";
     }
 
-    // To delete
-    @DeleteMapping("/test")
+    @DeleteMapping("test")
     public String deleteTest() {
         return "DELETE request successful";
     }
 
-    // To update
-    @PutMapping("/test")
+    @PutMapping("test")
     public String updateTest() {
         return "PUT request successful";
     }
-
     
-    @GetMapping("/all")
-    public String getAll(){
-        return "GET All request successful";
+    @GetMapping("all")
+    public String getAll() {
+    	return "GET all request successful";
     }
-
 }
 
